28f4b5ccdff4707ca72c16ddd8fd97f56e172a9a
chenxixian <chenxixian@gmail.com>
2019-06-29 23:30:56 +0800

    change commit

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-#echo "" > log/forever.log 
-forever start -l  log/forever.log --append -e log/error.log --append deploy.js
+#echo "" > ./log/forever.log 
+forever start -l  ./log/forever.log --append -e ./log/error.log --append deploy.js
